2d9bb251801132c3ef31b79104a26486b40f5f23
Blitzdude <joelkansala@gmail.com>
2018-10-09 21:13:18 +0300

    added boost

--- NetworkGameClient/src/Main.cpp
+++ NetworkGameClient/src/Main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <boost\asio.hpp>
 
 int main(int argc, void** argv)
 {
-	std::cout << "Hello World!" << std::endl;
+	boost::asio::io_context io;
+
+	boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
+	t.wait();
+
+	std::cout << "Hello, world!" << std::endl;
 
 	return 0;
 }
