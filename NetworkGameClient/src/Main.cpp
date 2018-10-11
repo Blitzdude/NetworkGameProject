40bdcfab00f594ac87517c690d35a5318e61d5a5
Blitzdude <joelkansala@gmail.com>
2018-10-11 14:58:32 +0300

    tutorial 4 on boost tutorial site. NOTE: DO NOT ADD boost_1_68_0 file to repository

--- NetworkGameClient/src/Main.cpp
+++ NetworkGameClient/src/Main.cpp
@@ -1,14 +1,58 @@
 #include <iostream>
-#include <boost\asio.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+class printer
+{
+public:
+	printer(boost::asio::io_context& io)
+		: m_timer(io, boost::asio::chrono::seconds(1)),
+		m_count(0)
+	{
+		m_timer.async_wait(boost::bind(&printer::print, this));
+	}
+	~printer()
+	{
+		std::cout << "The final count was: " << m_count << std::endl;
+	}
+
+	void print()
+	{
+		if (m_count < 5)
+		{
+			std::cout << m_count << std::endl;
+			++m_count;
+
+			m_timer.expires_at(m_timer.expiry() + boost::asio::chrono::seconds(1));
+			m_timer.async_wait(boost::bind(&printer::print, this));
+		}
+	}
+private:
+	boost::asio::steady_timer m_timer;
+	int m_count;
+
+};
+/*
+void print(const boost::system::error_code& /*e,
+	boost::asio::steady_timer* t, int* count)
+{
+	if (*count < 5)
+	{
+		std::cout << "Hello, world! " << *count << std::endl;
+		++(*count);
+		t->expires_at(t->expiry() + boost::asio::chrono::seconds(1));
+
+		t->async_wait(boost::bind(print,
+			boost::asio::placeholders::error, t, count));
+	}
+}
+*/
 
 int main(int argc, void** argv)
 {
 	boost::asio::io_context io;
-
-	boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
-	t.wait();
-
-	std::cout << "Hello, world!" << std::endl;
+	printer p(io);
+	io.run();
 
 	return 0;
 }
